77fdb96e2506737280382e5478711d06cb3ead5a
BabychOleksii <babych.oleksii@gmail.com>
2020-10-29 22:17:25 +0200

    p02 task

---
+++ p02.erl
@@ -0,0 +1,12 @@
+% P02 (*) Найти два последних элемента списка:
+% Пример:
+% 1> p02:but_last([a,b,c,d,e,f]).
+% [e,f]
+
+-module(p02).
+-export([but_last/1]).
+
+but_last([_,_]=X) ->
+  X;
+but_last([_|T]) ->
+  but_last(T).
