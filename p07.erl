ac6c89b96ad3d8fe8dbb1a2bbcc5a380b327b653
BabychOleksii <babych.oleksii@gmail.com>
2020-10-30 23:14:07 +0200

    p07 task

---
+++ p07.erl
@@ -0,0 +1,16 @@
+% P07 (**) Выровнять структуру с вложеными списками:
+% Пример:
+% 1> p07:flatten([a,[],[b,[c,d],e]]).
+% [a,b,c,d,e]
+
+-module(p07).
+-export([flatten/1]).
+
+flatten([[H|T]|E]) ->
+  flatten([H,T|E]);
+flatten([[]|T]) ->
+  flatten(T);
+flatten([H|T]) ->
+  [H| flatten(T)];
+flatten([]) ->
+  [].
